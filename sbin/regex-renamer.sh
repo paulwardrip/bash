741d664cde63be571f8769abefabd989fbadabd9
Paul Wardrip <pwardrip@gmail.com>
2015-03-02 18:43:11 -0500

    Default chmods for the scripts. Regex Renamer Usage Examples and Fixing Directory Handling.

--- sbin/regex-renamer.sh
+++ sbin/regex-renamer.sh
@@ -24,7 +24,7 @@ function __rnx_input {
 			;;
 		-*)
 			echo "Unknown Option $1"
-			exit 1
+			__rnx_usage
 			;;
 		*)
 			__rnx_directory="$1"
@@ -34,21 +34,35 @@ function __rnx_input {
 	done
 
 	if [ -z "$__rnx_pattern" ]; then
-		echo "No Pattern Specified"
-		exit 1	
+		__rnx_usage
 	fi
 
 	if [ -z "$__rnx_output" ]; then
-                echo "No Output Specified"
-                exit 1
-        fi
-
+        __rnx_usage
+    fi
 
 	if [ -z "$__rnx_directory" ]; then
 		__rnx_directory="."
 	fi
 
 	__rnx_rename
+}
+
+function __rnx_usage {
+    echo
+    echo "Usage: $0 -p <regex> -o <replacement> [directory]"
+    echo "  -p --pattern : a regex that matches the part of the files you want renamed."
+    echo "  -o --output  : replace the matched part of the filename with this."
+    echo "  -t --test    : perform a dry run (output filenames don't move anything)"
+    echo
+    echo "Recommendation: *** Use single quotes in pattern / output to prevent bash from manipulating the strings."
+    echo
+    echo "Example: rnx --test -p '(.*)([0-9]{2})(.*)' -o '$2--$1datafile$3' example/data"
+    echo " Dry Run: example/data/another-03.readme -> example/data/03--another-datafile.readme"
+    echo " Dry Run: example/data/infologs-01.logs -> example/data/01--infologs-datafile.logs"
+    echo " Dry Run: example/data/textfilenamed-00.txt -> example/data/00--textfilenamed-datafile.txt"
+    echo
+    exit 1
 }
 
 function __rnx_rename {
@@ -59,16 +73,18 @@ function __rnx_rename {
 	
 	
 	for __rnx_file in $__rnx_files; do
-		#IFS=$__rnx_oldifs
-		
-		if [[ "$__rnx_file" =~ $__rnx_pattern ]]; then
+		__rnx_base=`basename $__rnx_file`
+		__rnx_dir=`dirname $__rnx_file`
+
+		if [[ "$__rnx_base" =~ $__rnx_pattern ]]; then
 			if [ ${#BASH_REMATCH[@]} == "1" ]; then
-				__rnx_target="$( echo "$__rnx_file" | sed "s/$__rnx_pattern/$__rnx_output/gi" )"
+				__rnx_target="$__rnx_dir/$( echo "$__rnx_base" | sed "s/$__rnx_pattern/$__rnx_output/gi" )"
 			else
 				__rnx_target="$__rnx_output"
 				for (( __rnx_idx=1; __rnx_idx < ${#BASH_REMATCH[@]}; __rnx_idx++ )); do
 					__rnx_target="$( echo "$__rnx_target" | sed "s/\$$__rnx_idx/${BASH_REMATCH[__rnx_idx]}/g" )"
 				done
+				__rnx_target="$__rnx_dir/$__rnx_target"
 			fi
 			
 			if [ $__rnx_test == "1" ]; then
